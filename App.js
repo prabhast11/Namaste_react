68b9a6c8b1d115699b05e1ad67e4ef59668551bf
Prabhas Thakur <prabhasthakur@credenceanalytics.com>
2025-10-21 13:54:14 +0530

    nested structure in html using the js

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
-    const heading = React.createElement('h1', {}, "Hello world from React");
+    const heading = React.createElement('div', {id : 'parent'}, React.createElement('div', {id : 'child'}, 
+      [React.createElement('h1', {}, "Inside the first h1"),
+        React.createElement('h2', {}, "Inside the first h2"),]
+     ));
     const root = ReactDOM.createRoot(document.getElementById('root'));
     root.render(heading);
