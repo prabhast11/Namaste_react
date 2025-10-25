14856da07cc595aa991589b3405d5ae627d2fe08
Prabhas Thakur <prabhasthakur@credenceanalytics.com>
2025-10-25 22:14:28 +0530

    episode-02

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-    const heading = React.createElement('div', {id : 'parent'}, React.createElement('div', {id : 'child'}, 
+import React from "react";
+import ReactDOM from "react-dom"
+
+const heading = React.createElement('div', {id : 'parent'}, React.createElement('div', {id : 'child'}, 
       [React.createElement('h1', {}, "Inside the first h1"),
         React.createElement('h2', {}, "Inside the first h2"),]
      ));
-    const root = ReactDOM.createRoot(document.getElementById('root'));
-    root.render(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
---
+++ README.md
@@ -0,0 +1,8 @@
+# parcel
+
+- Dev Build
+- Local Server
+- HMR = Hot module replacement
+- File Watching Algorithm 
+- Caching - faster Builds
+- Image Optimization
